8f623802de77a3af969f993abea2bd13534ee3bf
Cybersphere <e.izukaev@elama.ru>
2019-05-17 18:12:49 +0300

    star db

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './error-indicator.css';
+
+export default class ErrorIndicator extends Component {
+  render() {
+    return (
+      <div className='error-indicator'>
+        <span className="boom">BOOM!</span>
+        <span>something has gone terribly wrong</span>
+        <span>but we already sent droids to fix it</span>
+      </div>
+    )
+  }
+}
